9e870507a8b2f48546c9aca84ee4dfc4cd9327db
Charlie Leopard <charles.leopard@outlook.com>
2020-10-08 16:54:31 +0100

    Update WelcomeMessage.js
    Added text

--- src/components/HomeSection/WelcomeMessage.js
+++ src/components/HomeSection/WelcomeMessage.js
@@ -11,9 +11,10 @@ class WelcomeMessage extends Component {
             <h2>Hi, I'm Charlie...</h2>
             <p>
               Welcome to my website, here you can learn more about me and
-              projects I am working on. I'm a CS graduate and a versatile
-              designer/developer with a particular interest in UX/UI, front-end
-              development, and ubiquitous technology.
+              projects I'm working on. I'm a CS graduate and a versatile
+              designer/developer with a particular interest in front-end
+              development, UX/UI, and ubiquitous technology. Currently
+              undertaking a masters in Advanced CS.
             </p>
           </div>
         </div>
