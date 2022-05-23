53e1f5d0f8816d125eddef5134ca6b0361228579
Charlie Leopard <charles.leopard@outlook.com>
2022-05-23 12:06:22 +0100

    Ignore lint on test

--- components/NavBar.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-
-import NavBar from "./NavBar";
-
-test("NavBar includes RetroFlect Name", () => {
-  render(<NavBar />);
-  const navigationBarTitle = screen.getByText(/Charlie Leopard/i);
-  expect(navigationBarTitle).not.toBeNull;
-})
--- src/components/NavBar.test.js
+++ src/components/NavBar.test.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { render, screen } from "@testing-library/react";
 
 import NavBar from "./NavBar";
@@ -7,4 +7,4 @@ test("NavBar includes title 'Charlie Leopard'", () => {
   render(<NavBar />);
   const navigationBarTitle = screen.getByText(/Charlie Leopard/i);
   expect(navigationBarTitle).not.toBeNull;
-})
+});
